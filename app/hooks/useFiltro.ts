00fe4bc050961d7a252bd2f606f6e06d3c15d71a
macastropi <macastropi04@gmail.com>
2025-11-10 09:11:24 -0500

    se comentaron algunas funciones en en useFiltro

--- app/hooks/useFiltro.ts
+++ app/hooks/useFiltro.ts
@@ -3,10 +3,10 @@ import { useState, useEffect, startTransition } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function useFiltro() {
-  const router = useRouter();
-  const params = useSearchParams();
+  const router = useRouter(); //permite leer los parámetros de búsqueda de la URL actual.
+  const params = useSearchParams();//permite cambiar las rutas mediante programación dentro del componente cliente.lee las rutas
 
-  const [q, setQ] = useState("");
+  const [q, setQ] = useState("");//Devuelve un valor con estado y una función para actualizarlo.
   const [cargo, setCargo] = useState("");
   const [estado, setEstado] = useState("");
   const [sort, setSort] = useState("");
@@ -24,7 +24,7 @@ export default function useFiltro() {
     const sortParam = params.get("sort") ?? "";
     const dirParam = (params.get("dir") as "asc" | "desc") ?? "asc";
 
-    startTransition(() => {
+    startTransition(() => { // Una función que provoca actualizaciones de estado que pueden aplazarse
       setQ(qParam);
       setCargo(cargoParam);
       setEstado(estadoParam);
@@ -35,13 +35,13 @@ export default function useFiltro() {
   }, [params]);
 
   //sincronixa la URL pero sin crear bucle, analiza que si cambian los filtros
-  useEffect(() => {
+  useEffect(() => { //Acepta una función que contiene código imperativo, posiblemente con efectos.
     if (isFirstLoad) {
       setIsFirstLoad(false);
       return;
     }
 
-    const usp = new URLSearchParams();
+    const usp = new URLSearchParams(); //La interfaz define métodos de utilidad para trabajar con la cadena de consulta de una URL
     if (q) usp.set("q", q);
     if (cargo) usp.set("cargo", cargo);
     if (estado) usp.set("estado", estado);
@@ -52,7 +52,7 @@ export default function useFiltro() {
     router.replace(`?${usp.toString()}`);
   }, [q, cargo, estado, page, sort, dir]);
 
-  const toggleSort = (campo: string) => {
+  const toggleSort = (campo: string) => { //permite alternar entre ordenar por una columna en dirección ascendente, luego descendente, y luego cambiar a una columna diferente
     if (sort === campo) {
       setDir((d) => (d === "asc" ? "desc" : "asc"));
     } else {
